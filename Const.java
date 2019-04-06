fc4af4fedd0b84487e98c17c42a192105d2d4a8e
starfy84 <dereck.tu@hotmail.com>
2019-04-06 00:37:22 -0400

    Add more constants

--- Const.java
+++ Const.java
@@ -1,3 +1,9 @@
 public final class Const{
 	public static final String HIGHSCORE_PATH = "saves/Highscores";
+	public static final String MAIN_MENU_PATH = "/Images/main menu/";
+	public static final String GAME_PATH = "/Images/game/";
+	private static final String CARD_PATH = "/Images/card/";
+	public static final String CARD_FRONT_PATH = "/Images/card/front/";
+	public static final String CARD_BACK_PATH = "/Images/card/back/";
+	public static final String TITLE = "The Changing of Dorian Gray";
 }
