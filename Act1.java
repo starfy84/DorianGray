8dcbd776f43aa8bd4a7b5c88a7c1b5463b9ca8a3
starfy84 <dereck.tu@hotmail.com>
2019-04-16 01:38:25 -0400

    Update Act1.java

--- Act1.java
+++ Act1.java
@@ -6,27 +6,22 @@ public class Act1 extends Game{
 
     
     Deck d = new Deck();
-    long start = System.currentTimeMillis();
     Const.DECK_CACHE.put("Act1:2:11-Act1:2:11",super.deckGenerator.genDeck(2,11,2,11));
     Const.DECK_CACHE.put("Act1:2:13-Act1:2:13",super.deckGenerator.genDeck(2,13,2,13));
     Const.DECK_CACHE.put("Act1:2:15-Act1:2:21",super.deckGenerator.genDeck(2,15,2,21));
     Const.DECK_CACHE.put("Act1:2:23-Act1:2:49",super.deckGenerator.genDeck(2,23,2,49));
-    System.out.println(((System.currentTimeMillis()-start)/1000.0) + " seconds");
     add(d,Const.DECK_CACHE.get("Act1:2:11-Act1:2:11"),Const.DECK_CACHE.get("Act1:2:13-Act1:2:13"),Const.DECK_CACHE.get("Act1:2:15-Act1:2:21"),Const.DECK_CACHE.get("Act1:2:23-Act1:2:49"));
     Const.DECK_CACHE.put("Act1: 2:11 2:13 2:15-2:21 2:23-2:49",d);
     
 
-    start = System.currentTimeMillis();
     d = new Deck();
     Const.DECK_CACHE.put("Act1:2:12-Act1:2:14",super.deckGenerator.genDeck(2,12,2,14));
     Const.DECK_CACHE.put("Act1:2:22-Act1:2:22",super.deckGenerator.genDeck(2,22,2,22));
     add(d,Const.DECK_CACHE.get("Act1:2:12-Act1:2:14"),Const.DECK_CACHE.get("Act1:2:22-Act1:2:22"));
     Const.DECK_CACHE.put("Act1: 2:12-2:14 2:22",d);
-    System.out.println(((System.currentTimeMillis()-start)/1000.0) + " seconds");
 
-    start = System.currentTimeMillis();
+
     Const.DECK_CACHE.put("Act1:2:51-Act1:2:53",super.deckGenerator.genDeck(2,51,2,53));
-    System.out.println(((System.currentTimeMillis()-start)/1000.0) + " seconds");
   }
   private void add(Deck d,Deck...l){
     for(Deck x:l)
